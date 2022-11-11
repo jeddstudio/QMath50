3116664454626cdeb5be82ebaabb80d45cf957b7
Jedd Wong <jeddstudio@gmail.com>
2022-11-11 23:22:34 +0800

    Game Link update  And Create Pages
    Added
    Advanced, Speed Run, Ultimate Speed Run Page(HTML files and main.py updated)

--- main.py
+++ main.py
@@ -166,10 +166,10 @@ def register():
     return render_template("register.html")
 
 
-################################################# Game #################################################
+################################################# Basic Game #################################################
 @app.route('/game', methods=['GET', 'POST'])
 @login_required
-def game_index():
+def game_basic():
 
     acc_id = session["user_id"]
     
@@ -241,7 +241,7 @@ def game_index():
                 db.execute("INSERT INTO profile (pofo_id, time, symbol, best_time) VALUES (?, ?, ?, ?)", acc_id, date_time, number_L, total_time)
                 print(f"Added SQL with no data in SQL: {total_time}s")
             
-            # If the time_record exists, update the data
+            # If the time_record exists
             else:
                 # Call out the best time in SQL, using MIN() method of SQL
                 best_time_held_db = db.execute("SELECT MIN(best_time) FROM profile WHERE pofo_id = ? AND symbol = ?", acc_id, number_L)
@@ -251,7 +251,7 @@ def game_index():
 
                 # Compare the best_time in SQL with the new total_time
                 if best_time_held > total_time:
-                    # Update profile db
+                    # INSERT a new data to db
                     db.execute("INSERT INTO profile (pofo_id, time, symbol, best_time) VALUES (?, ?, ?, ?)", acc_id, date_time, number_L, total_time)
                     print(f"New record!!!!!! {total_time}s")
                     print("Added to SQL")
@@ -275,18 +275,345 @@ def game_index():
             print("Wrong: ", guess, "Answer: ", answer, "=", number_L, number_R)
             pass
 
-
-
-
-        # best_time_db = db.execute("SELECT best_time FROM profile WHERE id = ?", acc_id)
-
-
-
-
     return render_template("game.html")
 
 
+################################################# Advanced Game #################################################
+@app.route('/advancedgame', methods=['GET', 'POST'])
+@login_required
+def game_advanced():
 
+    acc_id = session["user_id"]
+    
+    num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    random_list = random.sample(num_list, 9)
+
+    global number_L
+    global start_time
+    global number_R
+    global end_time
+    global counter
+
+    # Get number_L from form.js
+    if request.args.get('number_L'):
+
+        # If the user presses the refresh button in the middle of the game, it resets all Global value
+        if start_time != 0 and number_R > 1:
+            number_R = 1
+            start_time = 0
+            end_time = 0
+            counter = 1
+            number_L = int(request.args.get('number_L'))
+            return jsonify({'js_number_L': number_L})
+        # If not, normally to running the game
+        else:
+            number_L = int(request.args.get('number_L'))
+            return jsonify({'js_number_L': number_L})
+
+
+    # When user input a number and press Enter
+    # Get guess from form.js
+    if request.form.get('js_guess'):
+
+        answer = number_L * number_R
+        guess = int(request.form['js_guess'])
+
+        # Save the start time when user input(and press Enter) first answer
+        if counter == 1:
+            start_time = time.time()
+            print("START: ", start_time)
+
+        # When the last answer is correct, Game End 
+        if answer == guess and counter == 10:
+            # Calculating Time
+            end_time = time.time()
+            print("END: ", end_time)
+            total_time = float("{:.3f}".format((end_time - start_time)))
+            print(f"time_lapsed:{end_time} - {start_time}")
+            # Reset the Global values
+            counter = 1
+            number_R = 1
+
+            message = (f"Finished! \n Your time is: {total_time} seconds")
+            date_time = datetime.now()
+            print("Stopwatch: ", total_time)
+            # Show a game end message and Reset the num_R to 1
+
+
+            ############### SQL part ###############
+            # SQL only save the best_time record when the current time faster than SQL's fastest record
+
+            # Check if the best_time is exists in db or NOT
+            data_check = db.execute("SELECT best_time FROM profile WHERE EXISTS(SELECT * FROM profile WHERE pofo_id = ? AND symbol = ?)", acc_id, number_L)
+            # This is to check if data already exists. It will return [{'best_time': 4.441}, {'best_time': 9.717}]
+            # So if it len(data_check) == 0, it means there is no data
+
+            # If the best_time does NOT exist, use SQL INSERT to add a record
+            if len(data_check) == 0:
+                db.execute("INSERT INTO profile (pofo_id, time, symbol, best_time) VALUES (?, ?, ?, ?)", acc_id, date_time, number_L, total_time)
+                print(f"Added SQL with no data in SQL: {total_time}s")
+            
+            # If the time_record exists
+            else:
+                # Call out the best time in SQL, using MIN() method of SQL
+                best_time_held_db = db.execute("SELECT MIN(best_time) FROM profile WHERE pofo_id = ? AND symbol = ?", acc_id, number_L)
+                best_time_held = float(best_time_held_db[0]["MIN(best_time)"])
+                print(f"Your time is: {total_time}")
+                print(f"Best time found. Your best time in {number_L}: {best_time_held}")
+
+                # Compare the best_time in SQL with the new total_time
+                if best_time_held > total_time:
+                    # INSERT a new data to db
+                    db.execute("INSERT INTO profile (pofo_id, time, symbol, best_time) VALUES (?, ?, ?, ?)", acc_id, date_time, number_L, total_time)
+                    print(f"New record!!!!!! {total_time}s")
+                    print("Added to SQL")
+                else:
+                    pass
+            ############### SQL part ###############
+
+            return jsonify({'endGame': message, 'js_display_number': number_R})                
+
+        # else counter < 10: Keep running the game
+        elif answer == guess and counter < 10:
+            number_R = num_list[counter]
+            answer = number_L * number_R
+            counter += 1
+            print("Counter: ", counter)
+            # Refresh a new number to page
+            return jsonify({'js_display_number': number_R})
+       
+        # if the guess is wrong, just lets user keep guessing
+        else:
+            print("Wrong: ", guess, "Answer: ", answer, "=", number_L, number_R)
+            pass
+
+    return render_template("advancedgame.html")
+
+
+
+
+################################################# Speed Run Game #################################################
+@app.route('/speedrun', methods=['GET', 'POST'])
+@login_required
+def game_speedrun():
+
+    acc_id = session["user_id"]
+    
+    num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    random_list = random.sample(num_list, 9)
+
+    global number_L
+    global start_time
+    global number_R
+    global end_time
+    global counter
+
+    # Get number_L from form.js
+    if request.args.get('number_L'):
+
+        # If the user presses the refresh button in the middle of the game, it resets all Global value
+        if start_time != 0 and number_R > 1:
+            number_R = 1
+            start_time = 0
+            end_time = 0
+            counter = 1
+            number_L = int(request.args.get('number_L'))
+            return jsonify({'js_number_L': number_L})
+        # If not, normally to running the game
+        else:
+            number_L = int(request.args.get('number_L'))
+            return jsonify({'js_number_L': number_L})
+
+
+    # When user input a number and press Enter
+    # Get guess from form.js
+    if request.form.get('js_guess'):
+
+        answer = number_L * number_R
+        guess = int(request.form['js_guess'])
+
+        # Save the start time when user input(and press Enter) first answer
+        if counter == 1:
+            start_time = time.time()
+            print("START: ", start_time)
+
+        # When the last answer is correct, Game End 
+        if answer == guess and counter == 10:
+            # Calculating Time
+            end_time = time.time()
+            print("END: ", end_time)
+            total_time = float("{:.3f}".format((end_time - start_time)))
+            print(f"time_lapsed:{end_time} - {start_time}")
+            # Reset the Global values
+            counter = 1
+            number_R = 1
+
+            message = (f"Finished! \n Your time is: {total_time} seconds")
+            date_time = datetime.now()
+            print("Stopwatch: ", total_time)
+            # Show a game end message and Reset the num_R to 1
+
+
+            ############### SQL part ###############
+            # SQL only save the best_time record when the current time faster than SQL's fastest record
+
+            # Check if the best_time is exists in db or NOT
+            data_check = db.execute("SELECT best_time FROM profile WHERE EXISTS(SELECT * FROM profile WHERE pofo_id = ? AND symbol = ?)", acc_id, number_L)
+            # This is to check if data already exists. It will return [{'best_time': 4.441}, {'best_time': 9.717}]
+            # So if it len(data_check) == 0, it means there is no data
+
+            # If the best_time does NOT exist, use SQL INSERT to add a record
+            if len(data_check) == 0:
+                db.execute("INSERT INTO profile (pofo_id, time, symbol, best_time) VALUES (?, ?, ?, ?)", acc_id, date_time, number_L, total_time)
+                print(f"Added SQL with no data in SQL: {total_time}s")
+            
+            # If the time_record exists
+            else:
+                # Call out the best time in SQL, using MIN() method of SQL
+                best_time_held_db = db.execute("SELECT MIN(best_time) FROM profile WHERE pofo_id = ? AND symbol = ?", acc_id, number_L)
+                best_time_held = float(best_time_held_db[0]["MIN(best_time)"])
+                print(f"Your time is: {total_time}")
+                print(f"Best time found. Your best time in {number_L}: {best_time_held}")
+
+                # Compare the best_time in SQL with the new total_time
+                if best_time_held > total_time:
+                    # INSERT a new data to db
+                    db.execute("INSERT INTO profile (pofo_id, time, symbol, best_time) VALUES (?, ?, ?, ?)", acc_id, date_time, number_L, total_time)
+                    print(f"New record!!!!!! {total_time}s")
+                    print("Added to SQL")
+                else:
+                    pass
+            ############### SQL part ###############
+
+            return jsonify({'endGame': message, 'js_display_number': number_R})                
+
+        # else counter < 10: Keep running the game
+        elif answer == guess and counter < 10:
+            number_R = num_list[counter]
+            answer = number_L * number_R
+            counter += 1
+            print("Counter: ", counter)
+            # Refresh a new number to page
+            return jsonify({'js_display_number': number_R})
+       
+        # if the guess is wrong, just lets user keep guessing
+        else:
+            print("Wrong: ", guess, "Answer: ", answer, "=", number_L, number_R)
+            pass
+
+    return render_template("speedrun.html")
+
+
+################################################# Ultimate Speed Run Game #################################################
+@app.route('/ultispeedrun', methods=['GET', 'POST'])
+@login_required
+def game_ultispeedrun():
+
+    acc_id = session["user_id"]
+    
+    num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    random_list = random.sample(num_list, 9)
+
+    global number_L
+    global start_time
+    global number_R
+    global end_time
+    global counter
+
+    # Get number_L from form.js
+    if request.args.get('number_L'):
+
+        # If the user presses the refresh button in the middle of the game, it resets all Global value
+        if start_time != 0 and number_R > 1:
+            number_R = 1
+            start_time = 0
+            end_time = 0
+            counter = 1
+            number_L = int(request.args.get('number_L'))
+            return jsonify({'js_number_L': number_L})
+        # If not, normally to running the game
+        else:
+            number_L = int(request.args.get('number_L'))
+            return jsonify({'js_number_L': number_L})
+
+
+    # When user input a number and press Enter
+    # Get guess from form.js
+    if request.form.get('js_guess'):
+
+        answer = number_L * number_R
+        guess = int(request.form['js_guess'])
+
+        # Save the start time when user input(and press Enter) first answer
+        if counter == 1:
+            start_time = time.time()
+            print("START: ", start_time)
+
+        # When the last answer is correct, Game End 
+        if answer == guess and counter == 10:
+            # Calculating Time
+            end_time = time.time()
+            print("END: ", end_time)
+            total_time = float("{:.3f}".format((end_time - start_time)))
+            print(f"time_lapsed:{end_time} - {start_time}")
+            # Reset the Global values
+            counter = 1
+            number_R = 1
+
+            message = (f"Finished! \n Your time is: {total_time} seconds")
+            date_time = datetime.now()
+            print("Stopwatch: ", total_time)
+            # Show a game end message and Reset the num_R to 1
+
+
+            ############### SQL part ###############
+            # SQL only save the best_time record when the current time faster than SQL's fastest record
+
+            # Check if the best_time is exists in db or NOT
+            data_check = db.execute("SELECT best_time FROM profile WHERE EXISTS(SELECT * FROM profile WHERE pofo_id = ? AND symbol = ?)", acc_id, number_L)
+            # This is to check if data already exists. It will return [{'best_time': 4.441}, {'best_time': 9.717}]
+            # So if it len(data_check) == 0, it means there is no data
+
+            # If the best_time does NOT exist, use SQL INSERT to add a record
+            if len(data_check) == 0:
+                db.execute("INSERT INTO profile (pofo_id, time, symbol, best_time) VALUES (?, ?, ?, ?)", acc_id, date_time, number_L, total_time)
+                print(f"Added SQL with no data in SQL: {total_time}s")
+            
+            # If the time_record exists
+            else:
+                # Call out the best time in SQL, using MIN() method of SQL
+                best_time_held_db = db.execute("SELECT MIN(best_time) FROM profile WHERE pofo_id = ? AND symbol = ?", acc_id, number_L)
+                best_time_held = float(best_time_held_db[0]["MIN(best_time)"])
+                print(f"Your time is: {total_time}")
+                print(f"Best time found. Your best time in {number_L}: {best_time_held}")
+
+                # Compare the best_time in SQL with the new total_time
+                if best_time_held > total_time:
+                    # INSERT a new data to db
+                    db.execute("INSERT INTO profile (pofo_id, time, symbol, best_time) VALUES (?, ?, ?, ?)", acc_id, date_time, number_L, total_time)
+                    print(f"New record!!!!!! {total_time}s")
+                    print("Added to SQL")
+                else:
+                    pass
+            ############### SQL part ###############
+
+            return jsonify({'endGame': message, 'js_display_number': number_R})                
+
+        # else counter < 10: Keep running the game
+        elif answer == guess and counter < 10:
+            number_R = num_list[counter]
+            answer = number_L * number_R
+            counter += 1
+            print("Counter: ", counter)
+            # Refresh a new number to page
+            return jsonify({'js_display_number': number_R})
+       
+        # if the guess is wrong, just lets user keep guessing
+        else:
+            print("Wrong: ", guess, "Answer: ", answer, "=", number_L, number_R)
+            pass
+
+    return render_template("ultispeedrun.html")
 
 
 
